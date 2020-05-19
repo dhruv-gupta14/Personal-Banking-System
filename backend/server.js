225afe2bad6e15f5eb061c8653b10ecdd04e0de0
Lakshay Sharma <laksh1013shar@gmail.com>
2020-05-20 00:27:06 +0530

    login endpoint added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const mongo = require("./config/keys")
+const mongo = require("./config/keys");
 const app = express();
+const passport = require("passport");
+const userRoutes = require("./routes/api/users");
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
 // DB Config
 const db = mongo.mongoURI;
 // Connect to MongoDB
-mongoose.connect(db,{ 
+mongoose
+  .connect(db, {
     useNewUrlParser: true,
-    useUnifiedTopology: true   // for deprecation warning 
-})
-.then(() => console.log("MongoDB successfully connected"))
-.catch(err => console.log(err));
+    useUnifiedTopology: true, // for deprecation warning
+  })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", userRoutes);
 
 const port = process.env.PORT || 3000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
